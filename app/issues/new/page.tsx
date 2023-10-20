9cfdfc6e7ddadcdfd50718c8d43e5ee2c207cc2d
filipjaruska <filip.jaruska@gmail.com>
2023-10-20 22:15:14 +0200

    Disable SSR for MarkDown Editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,23 @@
 'use client';
-import { Button, Callout, Text, TextField } from '@radix-ui/themes'
-import SimpleMDE from 'react-simplemde-editor';
-import { useForm, Controller } from 'react-hook-form';
+import { Button, Callout, TextField } from '@radix-ui/themes';
+import dynamic from 'next/dynamic';
+import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
+import { createIssueSchema } from '@/app/validationSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
 import axios from 'axios';
-import "easymde/dist/easymde.min.css"
+import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { createIssueSchema } from '@/app/validationSchema';
-import ErrorMessage from '@/app/components/ErrorMessage';
+import { Controller, useForm } from 'react-hook-form';
 import { z } from 'zod';
-import Spinner from '@/app/components/Spinner';
+
+
+const SimpleMDE = dynamic(() =>
+    import('react-simplemde-editor'),
+    { ssr: false }
+);
+
 type IssueForm = z.infer<typeof createIssueSchema>;
 
 

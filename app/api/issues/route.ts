dddfc65e535575b05d37fe52ac84ef4a5c1860b8
filipjaruska <filip.jaruska@gmail.com>
2023-10-20 20:18:47 +0200

    Show the issues and fixed API

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,7 +1,7 @@
-import { Status } from "@prisma/client";
+import { Status  } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import prisma from '@prisma/client'
 import { createIssueSchema } from "../../validationSchema";
+import prisma from "@/prisma/client";
 
 export async function POST(request: NextRequest) {
     const body = await request.json();

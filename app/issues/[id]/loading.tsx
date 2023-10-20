203892d81780ce28e6dff8e1bfafe2cb0d5ad1d4
filipjaruska <filip.jaruska@gmail.com>
2023-10-20 21:43:47 +0200

    Show issue details

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingNewIdPage = () => {
+    return (
+        <div>loading</div>
+    )
+}
+
+export default LoadingNewIdPage
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -22,7 +22,9 @@ const IssuesPage = async () => {
                     {issues.map(issue => (
                         <Table.Row key={issue.id}>
                             <TableCell>
-                                {issue.title}
+                                <Link href={`/issues/${issue.id}`}>
+                                    {issue.title}
+                                </Link>
                                 <div className='block md:hidden'>
                                     <IssueStatusBadge status={issue.status} />
                                 </div>

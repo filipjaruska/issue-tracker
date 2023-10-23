c4bc4eddafca516321fe183963f9547641343c09
filipjaruska <filip.jaruska@gmail.com>
2023-10-23 18:37:24 +0200

    Added Edit Button

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,9 +1,5 @@
-import ErrorMessage from "./ErrorMessage";
-import IssueStatusBadge from "./IssueStatusBadge";
-import Spinner from "./Spinner";
-
 export { default as Link } from './Link';
 export { default as Skeleton } from './Skeleton';
-export {ErrorMessage};
-export {IssueStatusBadge};
-export {Spinner};
+export { default as ErrorMessage} from "./ErrorMessage";
+export { default as IssueStatusBadge}from "./IssueStatusBadge";
+export { default as Spinner} from"./Spinner";
